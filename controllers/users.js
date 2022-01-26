816e84e65b2f0255e8d2b42688c511c116f5f13b
houhs <houhs@data4truth.com>
2022-01-26 15:09:29 +0800

    feat：处理数据库异步问题；实现验证用户是否存在功能

--- controllers/users.js
+++ controllers/users.js
@@ -1,24 +1,37 @@
 /*
  * @Author: your name
  * @Date: 2022-01-25 15:39:38
- * @LastEditTime: 2022-01-25 17:01:13
+ * @LastEditTime: 2022-01-26 15:08:22
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \nodeJSProject\controllers\users.js
  */
 
 const usersModel = require('../models/users')
-const signUp = (req, res, next) =>{
+
+const signUp = async (req, res, next) =>{
   const {username, password} = req.body
-  console.log('username',username)
-  usersModel.signup({
-    username,
-    password
-  })
-  // res.send('respond with a resource');     // 直接返回
-  // 使用模板返回
-  res.render('succ', {          // 第一个参数是使用的模板文件名；第二个参数是要返回的内容
-    data:JSON.stringify({username})
-  })
+
+  // 判断是否存在该用户名
+  let findUserRes = await usersModel.findUser(username)   // 存在时对象，不存在时null
+  if(findUserRes){
+    // 重复名，返回错误信息
+    res.render('fail', {          // 第一个参数是使用的模板文件名；第二个参数是要返回的内容
+      data:JSON.stringify({
+        message:'用户名已存在'
+      })
+    })
+  }else{
+    // 注册用户
+    let signupResult = await usersModel.signup({
+      username,
+      password
+    })
+    // 使用模板返回
+    res.render('succ', {          // 第一个参数是使用的模板文件名；第二个参数是要返回的内容
+      data:JSON.stringify({username})
+    })
+  }
+
 }
 exports.signUp = signUp
--- models/users.js
+++ models/users.js
@@ -1,20 +1,27 @@
 /*
  * @Author: your name
  * @Date: 2022-01-25 16:43:55
- * @LastEditTime: 2022-01-25 16:46:10
+ * @LastEditTime: 2022-01-26 14:59:42
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \nodeJSProject\models\users.js
  */
 const {Users} = require('../utils/db')
 
+// 注册
 const signup = ({username, password}) => {
-  // 传给数据库
+  // 传给数据库-异步操作
   const users = new Users({
     username,
     password
   })
-  users.save()    // 保存
+  return users.save()    // 保存
+}
+
+// 查询
+const findUser = (username) => {
+  return Users.findOne({username})
 }
 
 exports.signup = signup
+exports.findUser = findUser
